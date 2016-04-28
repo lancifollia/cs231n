[assign1] (calc svm gradient) regularization term fixed

assignment1/cs231n/classifiers/linear_svm.py
@@ -41,7 +41,7 @@ def svm_loss_naive(W, X, y, reg):
     # to be an average instead so we divide by num_train.
     loss /= num_train    
     dW /= num_train
-    # dW += dW * 2
+    dW += reg * W
 
     # Add regularization to the loss.
     loss += 0.5 * reg * np.sum(W * W)
@@ -103,7 +103,7 @@ def svm_loss_vectorized(W, X, y, reg):
     ind = ind - gt_mul
     dW = np.dot(X.transpose(), ind)
     dW /= X.shape[0]
-    dW += reg * np.sum(W)
+    dW += reg * W
     #############################################################################
     #                            END OF YOUR CODE                               #
     #############################################################################